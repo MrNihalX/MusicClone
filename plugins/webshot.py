0a03619932e302a4a5d78daf883bf61e23b49ef6
NOBITA_XD <101139721+NOBITAXDD@users.noreply.github.com>
2022-09-24 22:43:46 +0530

    Add files via upload

---
+++ plugins/webshot.py
@@ -0,0 +1,33 @@
+# Powered By @BikashHalder @AdityaHalder
+# ©️ Copy Right By Bikash Halder Or Aditya Halder
+# Any Problem To Report @Bgt_Chat or @AdityaDiscus
+# Bot Owner @BikashHalder Or @AdityaHalder
+
+from pyrogram import filters
+from pyrogram.types import Message
+
+from Bikash import app
+from Bikash.utils.errors import capture_err
+
+
+@app.on_message(filters.command("webss"))
+@capture_err
+async def take_ss(_, message: Message):
+    try:
+        if len(message.command) != 2:
+            return await message.reply_text(
+                "**» ɢɪᴠᴇ ᴀ ᴜʀʟ ᴛᴏ ғᴇᴛᴄʜ sᴄʀᴇᴇɴsʜᴏᴛ...**"
+            )
+        url = message.text.split(None, 1)[1]
+        m = await message.reply_text("**» ᴛʀʏɪɴɢ ᴛᴏ ᴛᴀᴋᴇ sᴄʀᴇᴇɴsʜᴏᴛ...**")
+        await m.edit("**» ᴜᴩʟᴏᴀᴅɪɴɢ ᴄᴀᴩᴛᴜʀᴇᴅ sᴄʀᴇᴇɴsʜᴏᴛ...**")
+        try:
+            await message.reply_photo(
+                photo=f"https://webshot.amanoteam.com/print?q={url}",
+                quote=False,
+            )
+        except TypeError:
+            return await m.edit("**» ɴᴏ sᴜᴄʜ ᴡᴇʙsɪᴛᴇ.**")
+        await m.delete()
+    except Exception as e:
+        await message.reply_text(str(e))
