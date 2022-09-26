a9e49019078a65776a3255c19cc8ee3521ee349e
NOBITA_XD <101139721+NOBITAXDD@users.noreply.github.com>
2022-09-26 07:42:59 +0530

    Update stop.py

--- plugins/stop.py
+++ plugins/stop.py
@@ -1,17 +1,16 @@
-# Powered By @BikashHalder @AdityaHalder
-# ©️ Copy Right By Bikash Halder Or Aditya Halder
-# Any Problem To Report @Bgt_Chat or @AdityaDiscus
-# Bot Owner @BikashHalder Or @AdityaHalder
-
+# Powered By NOBITA_XD 
+# ©️ Copy Right By NOBITA_XD 
+# Any Problem To Report NOBITA_XD 
+# Bot Owner NOBITA_XD 
 from pyrogram import filters
 from pyrogram.types import Message
 
-from Bikash.config import BANNED_USERS
-from Bikash.strings import get_command
-from Bikash import app
-from Bikash.core.call import Bikashh
-from Bikash.utils.database import set_loop
-from Bikash.utils.decorators import AdminRightsCheck
+from Nobita.config import BANNED_USERS
+from Nobita.strings import get_command
+from Nobita import app
+from Nobita.core.call import Nobita
+from Nobita.utils.database import set_loop
+from Nobita.utils.decorators import AdminRightsCheck
 
 # Commands
 STOP_COMMAND = get_command("STOP_COMMAND")
@@ -27,7 +26,7 @@ STOP_COMMAND = get_command("STOP_COMMAND")
 async def stop_music(cli, message: Message, _, chat_id):
     if not len(message.command) == 1:
         return await message.reply_text(_["general_2"])
-    await Bikashh.stop_stream(chat_id)
+    await Nobita.stop_stream(chat_id)
     await set_loop(chat_id, 0)
     await message.reply_text(
         _["bh_9"].format(message.from_user.mention)
